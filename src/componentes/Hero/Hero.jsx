4df7e56ba7f1f245e0cfd776b7497235058b5e00
laura riera <laura_marquezriera@hotmail.com>
2022-06-07 09:36:08 -0300

    hero

--- src/componentes/Hero/Hero.jsx
+++ src/componentes/Hero/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
   <div className="hero-overlay bg-opacity-10"></div>
   <div className="hero-content text-center text-gray-700-content">
     <div className="max-w-md">
-      <h1 className="mb-2 text-5xl font-bold">Bienvenido!!</h1>
-      <p className="mb-96">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <h1 className="mb-2 text-5xl font-bold">Bienvenidos a Emi Pasteleria!!</h1>
+      <p className="mb-96">Buscamos la excelencia en el producto y su proceso. Elaboramos cada pieza para seducirlos.</p>
     </div>
   </div>
 </div>

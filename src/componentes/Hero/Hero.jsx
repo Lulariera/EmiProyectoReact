fe7ab05c69be2a83ef8f37ee5f165597f101dd21
laura riera <laura_marquezriera@hotmail.com>
2022-06-07 10:41:48 -0300

    modificaciones

--- src/componentes/Hero/Hero.jsx
+++ src/componentes/Hero/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
   <div className="hero-overlay bg-opacity-10"></div>
   <div className="hero-content text-center text-gray-700-content">
     <div className="max-w-md">
-      <h1 className="mb-2 text-5xl font-bold">Bienvenidos a Emi !!</h1>
-      <p className="mb-96">Buscamos la excelencia en el producto y su proceso. Elaboramos cada pieza para seducirlos.</p>
+      <h1 className="mb-2 text-5xl font-bold">Bienvenidos!!</h1>
+      <p className="mb-96">En EMI buscamos la excelencia en el producto y su proceso y elaboramos cada pieza para seducirlos.</p>
     </div>
   </div>
 </div>
